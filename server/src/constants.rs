26254dc6b7e36426932e5a467e8ef27c5d874ba3
nathanonchain <nathanonchain@gmail.com>
2025-10-23 00:00:53 +0300

    Project Constants

---
+++ server/src/constants.rs
@@ -0,0 +1,16 @@
+// Cookie names
+pub const ACCESS_TOKEN_COOKIE: &str = "access_token";
+pub const REFRESH_TOKEN_COOKIE: &str = "refresh_token";
+
+// Collection names
+pub const USERS_COLLECTION: &str = "users";
+pub const OTPS_COLLECTION: &str = "otps";
+pub const SESSIONS_COLLECTION: &str = "sessions";
+
+// Token types
+pub const TOKEN_TYPE_ACCESS: &str = "access";
+pub const TOKEN_TYPE_REFRESH: &str = "refresh";
+
+// OTP purposes
+pub const OTP_PURPOSE_EMAIL_VERIFICATION: &str = "email_verification";
+pub const OTP_PURPOSE_PASSWORD_RESET: &str = "password_reset";
